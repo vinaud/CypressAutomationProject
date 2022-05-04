027c3e2647d825de8442164fb9dfbb0d5fe7a4a7
Vinaud <leyenasd@gmail.com>
2022-05-04 10:35:30 -0300

    Assertion with cypress

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -3,7 +3,9 @@
 describe('My First Test', () => {
     it('is my first test case', () => {
       cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
-      cy.get('.search-keyword').type("cucumber");
+      cy.get('.search-keyword').type("ca");
+      cy.wait(2000);
+      cy.get('.product:visible').should('have.length', 4);
       
     })
   })
