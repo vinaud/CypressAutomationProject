c42659127cca91d97832fa517e4f3132b8267042
Vinaud <leyenasd@gmail.com>
2022-05-04 10:06:02 -0300

    locating elements with cypress

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,9 @@
+/// <reference types="Cypress" />
+
 describe('My First Test', () => {
     it('is my first test case', () => {
       cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+      cy.get('.search-keyword').type("cucumber");
+      
     })
   })
