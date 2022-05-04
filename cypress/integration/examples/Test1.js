987e72ddbded1e8e54f5ca8a3ce237140ca65196
Vinaud <leyenasd@gmail.com>
2022-05-03 21:28:44 -0300

    basic page visit with cypress

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,5 @@
 describe('My First Test', () => {
-    it('Does not do much!', () => {
-      expect(true).to.equal(true)
+    it('is my first test case', () => {
+      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
     })
   })
