a40495d959eb47e841cbb5719ab2695cb1f5de46
Vinaud <leyenasd@gmail.com>
2022-05-05 16:45:08 -0300

    finding element without index

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -10,6 +10,17 @@ describe('My First Test', () => {
       //parent child chaining
       cy.get('.products').find('.product').should('have.length', 4);
       cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
-      
+
+      //another way to locate the element without index
+      cy.get('.products').find('.product').each(($el, index, $list) => {
+
+       const textVeg=$el.find('h4.product-name').text();
+
+       if(textVeg.includes("Cashews")){
+
+         cy.wrap($el).find('button').click();
+         
+       }
+      });
     })
   })
