fc66d54db526e623643e7d22cafe3772f3e1c7a1
Vinaud <leyenasd@gmail.com>
2022-05-05 15:01:13 -0300

    parent child chaining

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -6,6 +6,10 @@ describe('My First Test', () => {
       cy.get('.search-keyword').type("ca");
       cy.wait(2000);
       cy.get('.product:visible').should('have.length', 4);
+
+      //parent child chaining
+      cy.get('.products').find('.product').should('have.length', 4);
+      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
       
     })
   })
