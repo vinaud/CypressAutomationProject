60b77c431322ae3d153651cbfc9c2a7666585106
Vinaud <leyenasd@gmail.com>
2022-05-06 10:42:20 -0300

    aliasing

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -8,11 +8,12 @@ describe('My First Test', () => {
       cy.get('.product:visible').should('have.length', 4);
 
       //parent child chaining
-      cy.get('.products').find('.product').should('have.length', 4);
-      cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+      cy.get('.products').as('productLocator');
+      cy.get('@productLocator').find('.product').should('have.length', 4);
+      cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click();
 
       //another way to locate the element without index
-      cy.get('.products').find('.product').each(($el, index, $list) => {
+      cy.get('@productLocator').find('.product').each(($el, index, $list) => {
 
        const textVeg=$el.find('h4.product-name').text();
 
