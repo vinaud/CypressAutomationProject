7634511b0f4ddfb53dd2ee179cddb4b9eef50ffa
Vinaud <leyenasd@gmail.com>
2022-05-09 14:44:49 -0300

    visible and invisible elemnts

--- cypress/integration/examples/Test3.js
+++ cypress/integration/examples/Test3.js
@@ -14,6 +14,7 @@ describe('My Third Test', () => {
 
       //static dropdown
       cy.get('select').select('option2').should('have.value', 'option2');
+
       //autocoplete dynamic dropdown
       cy.get('#autocomplete').type('indi');
       cy.get('.ui-menu-item div').each(($el, index, $list) => {
@@ -22,6 +23,16 @@ describe('My Third Test', () => {
         }
       });
       cy.get('#autocomplete').should('have.value', 'India');
+
+      //visible and invisible verify
+      cy.get('#displayed-text').should('be.visible');
+      cy.get('#hide-textbox').click();
+      cy.get('#displayed-text').should('not.be.visible');
+      cy.get('#show-textbox').click();
+      cy.get('#displayed-text').should('be.visible');
+
+      //radio button verify
+
 
       });
 
