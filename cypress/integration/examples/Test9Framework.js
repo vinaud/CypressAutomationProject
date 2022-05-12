67324ef2bae12531db2d5c644bc7135dde11cb33
Vinaud <leyenasd@gmail.com>
2022-05-12 11:42:48 -0300

    varify fields and attributes

--- cypress/integration/examples/Test9Framework.js
+++ cypress/integration/examples/Test9Framework.js
@@ -1,5 +1,6 @@
 /// <reference types="Cypress" />
 
+
 describe('Framework test case', () => {
     before(function() {
         //retrieves data from example.json file on fixtures
@@ -12,5 +13,9 @@ describe('Framework test case', () => {
         cy.visit('https://rahulshettyacademy.com/angularpractice/');
         cy.get('input[name="name"]:nth-child(2)').type(this.data.name);
         cy.get('select').select(this.data.gender);
+        cy.get(':nth-child(4) > .ng-untouched').should('have.value', this.data.name);
+        //varify if field has attribute
+        cy.get('input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2');
+        cy.get('#inlineRadio3').should('be.disabled');
     })
   })
