8918b6abeb29144f54194810dc6b541cd221780a
Vinaud <leyenasd@gmail.com>
2022-05-13 10:41:20 -0300

    more assertions

--- cypress/integration/examples/Test9Framework.js
+++ cypress/integration/examples/Test9Framework.js
@@ -34,6 +34,14 @@ describe('Framework test case', () => {
         cy.contains('Checkout').click();
         cy.get('#country').type('India');
         cy.get('.suggestions > ul > li > a').click();
+        cy.get("#checkbox2").click({force: true});
+        cy.get('input[type="submit"]').click();
+
+        //this won't work:  cy.get('alert').should('have.text', 'Success! Thank you! Your order will be delivered in next few weeks :-).');
+        cy.get('.alert').then(function(element){
+            const actualtext = element.text();
+            expect(actualtext.includes('Success')).to.be.true
+        })
 
       
     })
