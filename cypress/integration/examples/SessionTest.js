c30c4b34f4b26f1dc063b9bc5447fbc5727fd8ab
Vinaud <leyenasd@gmail.com>
2022-05-18 15:28:14 -0300

    e2e script post auth with token

--- cypress/integration/examples/SessionTest.js
+++ cypress/integration/examples/SessionTest.js
@@ -10,6 +10,21 @@ describe('JWT Session tests', () =>{
                     window.localStorage.setItem('token', Cypress.env('token'));
                 }
             });
+
+            cy.get(".card-body button:last-of-type").eq(1).click();
+            cy.get("[routerlink*='cart'").click();
+            cy.contains("Checkout").click();
+            cy.get("[placeholder*='Country'").click();
+            cy.get("[placeholder*='Country'").type("ind");
+            cy.on('uncaught:exception', (err, runnable) => {return false})
+            cy.get(".ta-results button").each(($el, index, $list) => {
+                if($el.text() === " India"){
+                   cy.wrap($el).click({force:true});
+                }
+            })
+            cy.get(".action__submit").click();
+            cy.wait(2000);
+            cy.get(".order-summary button").click();
         });
     });
 });
