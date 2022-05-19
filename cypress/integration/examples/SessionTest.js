d7293e56de2171edd6080f69d0fb2c399801e96f
Vinaud <leyenasd@gmail.com>
2022-05-19 10:15:15 -0300

    downloading and asserting csv file with cypress

--- cypress/integration/examples/SessionTest.js
+++ cypress/integration/examples/SessionTest.js
@@ -1,7 +1,10 @@
 /// <reference types = "cypress" />
+const neatCSV = require('neat-csv');
+
+let productName;
 
 describe('JWT Session tests', () =>{
-    it('is logged through local storage', () => {
+    it('is logged through local storage', async () => {
         //custom command should treat the promise as it was not previous treated inside it
         cy.loginAPI().then(function(){
             //visit method can receive option, like the one setting the token to storage before loading the window
@@ -11,11 +14,16 @@ describe('JWT Session tests', () =>{
                 }
             });
 
+            cy.get(".card-body b").eq(1).then(function(element){
+                productName = element.text();
+            });
             cy.get(".card-body button:last-of-type").eq(1).click();
             cy.get("[routerlink*='cart'").click();
             cy.contains("Checkout").click();
             cy.get("[placeholder*='Country'").click();
-            cy.get("[placeholder*='Country'").type("ind");
+            cy.get("[placeholder*='Country'").type("i");
+            cy.get("[placeholder*='Country'").type("n");
+            cy.get("[placeholder*='Country'").type("d");
             cy.on('uncaught:exception', (err, runnable) => {return false})
             cy.get(".ta-results button").each(($el, index, $list) => {
                 if($el.text() === " India"){
@@ -25,6 +33,14 @@ describe('JWT Session tests', () =>{
             cy.get(".action__submit").click();
             cy.wait(2000);
             cy.get(".order-summary button").click();
+
+            cy.readFile(Cypress.config("fileServerFolder") + "\\cypress\\downloads\\order-invoice_leyenasd.csv").then(async function(text){
+                const csv = await neatCSV(text);
+                console.log(csv);
+                const actualProductCSV = csv[0]["Product Name"];
+                expect(actualProductCSV).to.be.equal(productName);
+            });
+            
         });
     });
 });
